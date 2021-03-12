55b618776ad58233a1ed94af0ce3a98de37a2499
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2021-03-12 11:00:18 -0800

    added server, connection and auth files

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/color-theory', {
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
